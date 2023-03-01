4e8c11b124a17874fd9bcc3f2d63f8e0e0eeeca9
Your Name <you@example.com>
2023-03-01 09:01:27 +0800

    add table

--- database/procurement.sql
+++ database/procurement.sql
@@ -16,28 +16,105 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`procurement` /*!40100 DEFAULT CHARACTER
 
 USE `procurement`;
 
-/*Table structure for table `ppmp_request_submission` */
+/*Table structure for table `pr_signatories` */
 
-DROP TABLE IF EXISTS `ppmp_request_submission`;
+DROP TABLE IF EXISTS `pr_signatories`;
 
-CREATE TABLE `ppmp_request_submission` (
+CREATE TABLE `pr_signatories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `employee_id` int(11) DEFAULT NULL,
-  `department_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `campus` int(11) DEFAULT NULL,
-  `allocated_budget` int(11) DEFAULT NULL,
-  `reason` longtext DEFAULT NULL,
+  `designation` varchar(255) DEFAULT NULL,
+  `title` varchar(55) DEFAULT NULL,
+  `amount` int(255) DEFAULT NULL,
+  `signatory_type` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `purchase_request` */
+
+DROP TABLE IF EXISTS `purchase_request`;
+
+CREATE TABLE `purchase_request` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `campus` int(11) DEFAULT NULL,
+  `project_code` int(255) DEFAULT NULL,
+  `fund_source_id` int(11) DEFAULT NULL,
+  `department_id` int(11) DEFAULT NULL,
+  `pr_no` varchar(55) DEFAULT NULL,
+  `responsibility_center_code` varchar(55) DEFAULT NULL,
+  `purpose` text DEFAULT NULL,
+  `printed_name` int(55) DEFAULT NULL,
+  `approving_officer` int(11) DEFAULT NULL,
+  `designation` varchar(255) DEFAULT NULL,
   `status` int(11) DEFAULT 0,
   `remark` varchar(255) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  `updated_at` date DEFAULT NULL,
-  `deleted_at` date DEFAULT NULL,
+  `approved_at` datetime DEFAULT NULL,
+  `disapproved_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `ppmp_request_submission` */
+/*Table structure for table `purchase_request_items` */
 
-insert  into `ppmp_request_submission`(`id`,`employee_id`,`department_id`,`campus`,`allocated_budget`,`reason`,`status`,`remark`,`created_at`,`updated_at`,`deleted_at`) values (1,1000410,107,1,88,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',1,NULL,'2023-02-20','2023-02-20',NULL);
+DROP TABLE IF EXISTS `purchase_request_items`;
+
+CREATE TABLE `purchase_request_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `campus` int(11) DEFAULT NULL,
+  `pr_no` varchar(55) DEFAULT '0',
+  `project_code` int(11) DEFAULT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `specification` text DEFAULT NULL,
+  `file_name` varchar(55) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `routing_slip` */
+
+DROP TABLE IF EXISTS `routing_slip`;
+
+CREATE TABLE `routing_slip` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pr_no` varchar(55) DEFAULT NULL,
+  `employee_id` int(55) DEFAULT NULL,
+  `role` int(11) DEFAULT NULL,
+  `campus` int(11) DEFAULT NULL,
+  `activity` int(11) DEFAULT NULL,
+  `date_received` datetime DEFAULT NULL,
+  `date_released` datetime DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `signed_purchase_request` */
+
+DROP TABLE IF EXISTS `signed_purchase_request`;
+
+CREATE TABLE `signed_purchase_request` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pr_no` varchar(55) DEFAULT NULL,
+  `campus` int(55) DEFAULT NULL,
+  `department_id` int(55) DEFAULT NULL,
+  `employee_id` int(55) DEFAULT NULL,
+  `file_name` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
